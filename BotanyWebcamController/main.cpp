77098fa86800a6a1c1a4b78ca1187a7c97f9069b
Ziliang Guo <zguo2@wisc.edu>
2016-01-03 21:03:31 -0600

    Add initial support for multiple cameras, still need to sort generated files. Add the library dependencies that somehow got lost. Target Windows version to latest 10.

--- BotanyWebcamController/main.cpp
+++ BotanyWebcamController/main.cpp
@@ -39,291 +39,316 @@
 #include <stdint.h>
 
 #define CAMERA_NAME L"Logitech HD Webcam C310"
+#define MAX_CAMERA_CNT 3
 
 #define INIT_CMD 'a'
 #define TAKE_PHOTO_CMD 't'
 #define PIC_DONE_CMD 'd'
 
-IMFMediaSource *ppSource;
-IMFSourceReader *pSourceReader;
+IMFMediaSource *pSource[MAX_CAMERA_CNT];
+IMFSourceReader *pSourceReader[MAX_CAMERA_CNT];
 IWICImagingFactory *pFactory = nullptr;
 
 wchar_t picName[MAX_PATH];
 
+int cameraCount = 0;
+
 template <class T> void SafeRelease(T **ppT)
 {
-	if (*ppT)
-	{
-		(*ppT)->Release();
-		*ppT = nullptr;
-	}
+    if (*ppT)
+    {
+        (*ppT)->Release();
+        *ppT = nullptr;
+    }
 }
 
-void CreateImage(BYTE* pData, DWORD currentLength)
+void CreateImage(BYTE* pData, DWORD currentLength, int cameraIndex)
 {
-	HRESULT hr;
-	SYSTEMTIME systemTime;
-	IWICBitmap *bitmap;
-	IWICStream *pStream = nullptr;
-	IWICBitmapEncoder *pEncoder = nullptr;
-	IWICBitmapFrameEncode *pFrameEncode = nullptr;
-	WICPixelFormatGUID pPixelFormat;
+    HRESULT hr;
+    SYSTEMTIME systemTime;
+    IWICBitmap *bitmap;
+    IWICStream *pStream = nullptr;
+    IWICBitmapEncoder *pEncoder = nullptr;
+    IWICBitmapFrameEncode *pFrameEncode = nullptr;
+    WICPixelFormatGUID pPixelFormat;
 
-	GetLocalTime(&systemTime);
-	//Year-Month-Date-Hour-Minute-Second-Camera
-	StringCbPrintf(picName, MAX_PATH, L"%d-%d-%d-%d-%d-%d-A.jpg", systemTime.wYear, systemTime.wMonth, systemTime.wDay, systemTime.wHour, systemTime.wMinute, systemTime.wSecond);
-	hr = pFactory->CreateBitmapFromMemory(
-		640, 480,
-		GUID_WICPixelFormat24bppRGB,
-		1920, currentLength, pData, &bitmap);
+    GetLocalTime(&systemTime);
+    // Year-Month-Date-Hour-Minute-Second
+    StringCbPrintf(picName, MAX_PATH, L"%d-%d-%d-%d-%d-%d.jpg", systemTime.wYear, systemTime.wMonth, systemTime.wDay, systemTime.wHour, systemTime.wMinute, systemTime.wSecond);
+    hr = pFactory->CreateBitmapFromMemory(
+        640, 480,
+        GUID_WICPixelFormat24bppRGB,
+        1920, currentLength, pData, &bitmap);
 
-	hr = pFactory->CreateStream(&pStream);
-	hr = pStream->InitializeFromFilename(picName, GENERIC_WRITE);
+    hr = pFactory->CreateStream(&pStream);
+    hr = pStream->InitializeFromFilename(picName, GENERIC_WRITE);
 
-	hr = pFactory->CreateEncoder(GUID_ContainerFormatJpeg, nullptr, &pEncoder);
-	hr = pEncoder->Initialize(pStream, WICBitmapEncoderNoCache);
-	hr = pEncoder->CreateNewFrame(&pFrameEncode, nullptr);
-	hr = pFrameEncode->Initialize(nullptr);
-	hr = pFrameEncode->SetSize(640, 480);
-	bitmap->GetPixelFormat(&pPixelFormat);
-	hr = pFrameEncode->SetPixelFormat(&pPixelFormat);
+    hr = pFactory->CreateEncoder(GUID_ContainerFormatJpeg, nullptr, &pEncoder);
+    hr = pEncoder->Initialize(pStream, WICBitmapEncoderNoCache);
+    hr = pEncoder->CreateNewFrame(&pFrameEncode, nullptr);
+    hr = pFrameEncode->Initialize(nullptr);
+    hr = pFrameEncode->SetSize(640, 480);
+    bitmap->GetPixelFormat(&pPixelFormat);
+    hr = pFrameEncode->SetPixelFormat(&pPixelFormat);
 
-	hr = pFrameEncode->WriteSource(bitmap, NULL);
-	hr = pFrameEncode->Commit();
+    hr = pFrameEncode->WriteSource(bitmap, NULL);
+    hr = pFrameEncode->Commit();
 
-	hr = pEncoder->Commit();
-	pFrameEncode->Release();
-	pEncoder->Release();
-	bitmap->Release();
-	pStream->Release();
+    hr = pEncoder->Commit();
+    pFrameEncode->Release();
+    pEncoder->Release();
+    bitmap->Release();
+    pStream->Release();
 }
 
 int main()
 {
-	HRESULT hr;
+    HRESULT hr;
 
-	UINT32 count = 0;
-	uint32_t index;
-	DWORD streamCount = 0;
-	DWORD streamIndex;
-	DWORD streamFlags;
-	LONGLONG timestamp;
+    UINT32 count = 0;
+    uint32_t index;
+    DWORD streamCount = 0;
+    DWORD streamIndex;
+    DWORD streamFlags;
+    LONGLONG timestamp;
 
-	IMFAttributes *pConfig = nullptr;
-	IMFAttributes *pSourceReaderConfig = nullptr;
-	IMFActivate **ppDevices = nullptr;
-	IMFPresentationDescriptor* pSourcePD = NULL;
+    int cameraIndex;
 
-	WORD requestVersion;
-	WSADATA wsaData;
-	SOCKET udpSocket;
-	struct sockaddr_in targetAddress;
-	struct sockaddr_in service;
+    IMFAttributes *pConfig = nullptr;
+    IMFAttributes *pSourceReaderConfig = nullptr;
+    IMFActivate **ppDevices = nullptr;
+    //IMFPresentationDescriptor* pSourcePD = NULL;
 
-	char sendBuffer;
-	int result;
+    WORD requestVersion;
+    WSADATA wsaData;
+    SOCKET udpSocket;
+    struct sockaddr_in targetAddress;
+    struct sockaddr_in service;
 
-	requestVersion = MAKEWORD(2, 2);
-	if (WSAStartup(requestVersion, &wsaData) != 0)
-		return -1;
+    char sendBuffer;
+    int result;
 
-	udpSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    requestVersion = MAKEWORD(2, 2);
+    if (WSAStartup(requestVersion, &wsaData) != 0)
+        return -1;
 
-	if (udpSocket == INVALID_SOCKET)
-	{
-		WSACleanup();
-		return -1;
-	}
+    udpSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-	service.sin_family = AF_INET;
-	InetPton(AF_INET, L"127.0.0.1", &(service.sin_addr.s_addr));
-	service.sin_port = htons(10001);
+    if (udpSocket == INVALID_SOCKET)
+    {
+        WSACleanup();
+        return -1;
+    }
 
-	if (bind(udpSocket, (SOCKADDR *)&service, sizeof(service)) == SOCKET_ERROR)
-	{
-		WSACleanup();
-		return -1;
-	}
+    service.sin_family = AF_INET;
+    InetPton(AF_INET, L"127.0.0.1", &(service.sin_addr.s_addr));
+    service.sin_port = htons(10001);
 
-	hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
-	if (FAILED(hr))
-	{
-		WSACleanup();
-		return -1;
-	}
+    if (bind(udpSocket, (SOCKADDR *)&service, sizeof(service)) == SOCKET_ERROR)
+    {
+        WSACleanup();
+        return -1;
+    }
 
-	MFStartup(MF_VERSION);
+    hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+    if (FAILED(hr))
+    {
+        WSACleanup();
+        return -1;
+    }
 
-	hr = MFCreateAttributes(&pConfig, 1);
-	if (FAILED(hr))
-		goto Cleanup;
+    MFStartup(MF_VERSION);
 
-	hr = pConfig->SetGUID(
-		MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
-		MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID
-		);
+    hr = MFCreateAttributes(&pConfig, 1);
+    if (FAILED(hr))
+        goto Cleanup;
 
-	if (FAILED(hr))
-		goto Cleanup;
+    hr = pConfig->SetGUID(
+        MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
+        MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID
+        );
 
-	hr = MFEnumDeviceSources(pConfig, &ppDevices, &count);
+    if (FAILED(hr))
+        goto Cleanup;
 
-	for (index = 0; index < count; ++index)
-	{
-		WCHAR *szFriendlyName = NULL;
-		UINT32 cchName;
+    hr = MFEnumDeviceSources(pConfig, &ppDevices, &count);
 
-		hr = ppDevices[index]->GetAllocatedString(
-			MF_DEVSOURCE_ATTRIBUTE_FRIENDLY_NAME,
-			&szFriendlyName, &cchName);
+    for (index = 0; (index < count) && (cameraCount < 3); ++index)
+    {
+        WCHAR *szFriendlyName = NULL;
+        UINT32 cchName;
 
-		if (SUCCEEDED(hr))
-		{
-			//wprintf_s(L"%s\n", szFriendlyName);
-			if (wcscmp(szFriendlyName, CAMERA_NAME) == 0)
-			{
-				ppDevices[index]->ActivateObject(IID_PPV_ARGS(&ppSource));
-				CoTaskMemFree(szFriendlyName);
-				break;
-			}
-		}
-		CoTaskMemFree(szFriendlyName);
-	}
+        hr = ppDevices[index]->GetAllocatedString(
+            MF_DEVSOURCE_ATTRIBUTE_FRIENDLY_NAME,
+            &szFriendlyName, &cchName);
+
+        if (SUCCEEDED(hr))
+        {
+            //wprintf_s(L"%s\n", szFriendlyName);
+            if (wcscmp(szFriendlyName, CAMERA_NAME) == 0)
+            {
+                ppDevices[index]->ActivateObject(IID_PPV_ARGS(&pSource[cameraCount]));
+                CoTaskMemFree(szFriendlyName);
+                ++cameraCount;
+                break;
+            }
+        }
+        CoTaskMemFree(szFriendlyName);
+    }
+
+    if (cameraCount == 0)
+    {
+        goto Cleanup;
+    }
+
+    hr = MFCreateAttributes(&pSourceReaderConfig, 1);
+    hr = pSourceReaderConfig->SetUINT32(MF_LOW_LATENCY, TRUE);
+
+    for (cameraIndex = 0; cameraIndex < cameraCount; ++cameraIndex)
+    {
+        //hr = pSource[cameraIndex]->CreatePresentationDescriptor(&pSourcePD);
+        //if (FAILED(hr))
+        //    goto Cleanup;
+
+        hr = MFCreateSourceReaderFromMediaSource(pSource[cameraIndex], pSourceReaderConfig, &pSourceReader[cameraIndex]);
+
+        DWORD mediaTypeIndex = 0;
+        hr = S_OK;
+        IMFMediaType *pType = NULL;
+        while (SUCCEEDED(hr))
+        {
+            hr = pSourceReader[cameraIndex]->GetNativeMediaType(MF_SOURCE_READER_FIRST_VIDEO_STREAM, mediaTypeIndex, &pType);
+            if (hr == MF_E_NO_MORE_TYPES)
+            {
+                hr = S_OK;
+                break;
+            }
+
+            ++mediaTypeIndex;
+
+            if (SUCCEEDED(hr))
+            {
+                GUID mediaMajorGUID;
+                GUID mediaSubGUID;
+
+                UINT32 height;
+                UINT32 width;
+                UINT32 frameRateNum;
+                UINT32 frameRateDenum;
+
+                pType->GetMajorType(&mediaMajorGUID);
+                pType->GetGUID(MF_MT_SUBTYPE, &mediaSubGUID);
+
+                if (!IsEqualGUID(mediaSubGUID, MFVideoFormat_RGB24))
+                {
+                    pType->Release();
+                    continue;
+                }
+
+                hr = MFGetAttributeSize(pType, MF_MT_FRAME_SIZE, &width, &height);
+                //wprintf_s(L"%d by %d\n", width, height);
+                if (width != 640 || height != 480)
+                {
+                    pType->Release();
+                    continue;
+                }
 
 
-	hr = ppSource->CreatePresentationDescriptor(&pSourcePD);
-	if (FAILED(hr))
-		goto Cleanup;
+                MFGetAttributeRatio(pType, MF_MT_FRAME_RATE, &frameRateNum, &frameRateDenum);
 
-	hr = MFCreateAttributes(&pSourceReaderConfig, 1);
-	hr = pSourceReaderConfig->SetUINT32(MF_LOW_LATENCY, TRUE);
+                if (frameRateNum != 30 || frameRateDenum != 1)
+                {
+                    pType->Release();
+                    continue;
+                }
 
-	hr = MFCreateSourceReaderFromMediaSource(ppSource, pSourceReaderConfig, &pSourceReader);
+                hr = pSourceReader[cameraIndex]->SetCurrentMediaType(MF_SOURCE_READER_FIRST_VIDEO_STREAM, nullptr, pType);
+                pType->Release();
+                break;
+            }
+        }
+    }
 
-	DWORD mediaTypeIndex = 0;
-	hr = S_OK;
-	IMFMediaType *pType = NULL;
-	while (SUCCEEDED(hr))
-	{
-		hr = pSourceReader->GetNativeMediaType(MF_SOURCE_READER_FIRST_VIDEO_STREAM, mediaTypeIndex, &pType);
-		if (hr == MF_E_NO_MORE_TYPES)
-		{
-			hr = S_OK;
-			break;
-		}
+    hr = CoCreateInstance(
+        CLSID_WICImagingFactory,
+        NULL,
+        CLSCTX_INPROC_SERVER,
+        IID_IWICImagingFactory,
+        (LPVOID*)&pFactory
+        );
 
-		++mediaTypeIndex;
+    IMFSample *imfSample = nullptr;
 
-		if (SUCCEEDED(hr))
-		{
-			GUID mediaMajorGUID;
-			GUID mediaSubGUID;
+    memset(&targetAddress, 0, sizeof(struct sockaddr_in));
+    sendBuffer = INIT_CMD;
+    targetAddress.sin_family = AF_INET;
+    InetPton(AF_INET, L"127.0.0.1", &(targetAddress.sin_addr.s_addr));
+    targetAddress.sin_port = htons(10000);
+    result = sendto(udpSocket, (const char*)&sendBuffer, 1, 0, (const sockaddr*)&targetAddress, sizeof(targetAddress));
 
-			UINT32 height;
-			UINT32 width;
-			UINT32 frameRateNum;
-			UINT32 frameRateDenum;
+    while (true)
+    {
+        DWORD currentLength;
+        char recBuffer;
+        int fromLen;
 
-			pType->GetMajorType(&mediaMajorGUID);
-			pType->GetGUID(MF_MT_SUBTYPE, &mediaSubGUID);
+        memset(&targetAddress, 0, sizeof(struct sockaddr_in));
 
-			if (!IsEqualGUID(mediaSubGUID, MFVideoFormat_RGB24))
-			{
-				pType->Release();
-				continue;
-			}
+        fromLen = sizeof(targetAddress);
+        recvfrom(udpSocket, &recBuffer, 1, 0, (sockaddr*)&targetAddress, &fromLen);
 
-			hr = MFGetAttributeSize(pType, MF_MT_FRAME_SIZE, &width, &height);
-			//wprintf_s(L"%d by %d\n", width, height);
-			if (width != 640 || height != 480)
-			{
-				pType->Release();
-				continue;
-			}
+        if (recBuffer == TAKE_PHOTO_CMD)
+        {
+            for (cameraIndex = 0; cameraIndex < cameraCount; ++cameraIndex)
+            {
+                while (true)
+                {
+                    IMFMediaBuffer *mediaBuffer = nullptr;
+                    BYTE *pData = nullptr;
 
+                    hr = pSourceReader[cameraIndex]->ReadSample(MF_SOURCE_READER_FIRST_VIDEO_STREAM, 0, &streamIndex, &streamFlags, &timestamp, &imfSample);
+                    if (!imfSample)
+                        continue;
 
-			MFGetAttributeRatio(pType, MF_MT_FRAME_RATE, &frameRateNum, &frameRateDenum);
+                    imfSample->ConvertToContiguousBuffer(&mediaBuffer);
+                    mediaBuffer->Lock(&pData, NULL, &currentLength);
 
-			if (frameRateNum != 30 || frameRateDenum != 1)
-			{
-				pType->Release();
-				continue;
-			}
+                    if (currentLength == 0)
+                        continue;
 
-			hr = pSourceReader->SetCurrentMediaType(MF_SOURCE_READER_FIRST_VIDEO_STREAM, nullptr, pType);
-			pType->Release();
-			break;
-		}
-	}
+                    CreateImage(pData, currentLength, cameraIndex);
 
-	hr = CoCreateInstance(
-		CLSID_WICImagingFactory,
-		NULL,
-		CLSCTX_INPROC_SERVER,
-		IID_IWICImagingFactory,
-		(LPVOID*)&pFactory
-		);
+                    mediaBuffer->Unlock();
+                    mediaBuffer->Release();
+                    imfSample->Release();
 
-	IMFSample *imfSample = nullptr;
+                    pSourceReader[cameraIndex]->Flush(MF_SOURCE_READER_FIRST_VIDEO_STREAM);
+                    break;
+                }
+            }
 
-	memset(&targetAddress, 0, sizeof(struct sockaddr_in));
-	sendBuffer = INIT_CMD;
-	targetAddress.sin_family = AF_INET;
-	InetPton(AF_INET, L"127.0.0.1", &(targetAddress.sin_addr.s_addr));
-	targetAddress.sin_port = htons(10000);
-	result = sendto(udpSocket, (const char*)&sendBuffer, 1, 0, (const sockaddr*)&targetAddress, sizeof(targetAddress));
-
-	while (true)
-	{
-		DWORD currentLength;
-		char recBuffer;
-		int fromLen;
-
-		memset(&targetAddress, 0, sizeof(struct sockaddr_in));
-
-		fromLen = sizeof(targetAddress);
-		recvfrom(udpSocket, &recBuffer, 1, 0, (sockaddr*)&targetAddress, &fromLen);
-
-		if (recBuffer == TAKE_PHOTO_CMD)
-		{
-			while (true)
-			{
-				IMFMediaBuffer *mediaBuffer = nullptr;
-				BYTE *pData = nullptr;
-
-				hr = pSourceReader->ReadSample(MF_SOURCE_READER_FIRST_VIDEO_STREAM, 0, &streamIndex, &streamFlags, &timestamp, &imfSample);
-				if (!imfSample)
-					continue;
-
-				imfSample->ConvertToContiguousBuffer(&mediaBuffer);
-				mediaBuffer->Lock(&pData, NULL, &currentLength);
-
-				if (currentLength == 0)
-					continue;
-
-				CreateImage(pData, currentLength);
-
-				mediaBuffer->Unlock();
-				mediaBuffer->Release();
-				imfSample->Release();
-
-				pSourceReader->Flush(MF_SOURCE_READER_FIRST_VIDEO_STREAM);
-				sendBuffer = PIC_DONE_CMD;
-				result = sendto(udpSocket, (const char*)&sendBuffer, 1, 0, (const sockaddr*)&targetAddress, sizeof(targetAddress));
-				break;
-			}
-		}
-	}
+            sendBuffer = PIC_DONE_CMD;
+            result = sendto(udpSocket, (const char*)&sendBuffer, 1, 0, (const sockaddr*)&targetAddress, sizeof(targetAddress));
+        }
+    }
 
 Cleanup:
-	if (ppDevices != nullptr)
-		CoTaskMemFree(ppDevices);
-	if (pConfig != nullptr)
-		pConfig->Release();
-	if (pFactory != nullptr)
-		pFactory->Release();
-	WSACleanup();
-	MFShutdown();
+    for (cameraIndex = 0; cameraIndex < cameraCount; ++cameraIndex)
+    {
+        pSourceReader[cameraIndex]->Release();
+        pSourceReader[cameraIndex] = nullptr;
+        pSource[cameraIndex]->Release();
+        pSource[cameraIndex] = nullptr;
+    }
 
-	return 0;
+    if (ppDevices != nullptr)
+        CoTaskMemFree(ppDevices);
+    if (pConfig != nullptr)
+        pConfig->Release();
+    if (pFactory != nullptr)
+        pFactory->Release();
+    WSACleanup();
+    MFShutdown();
+
+    return 0;
 }
